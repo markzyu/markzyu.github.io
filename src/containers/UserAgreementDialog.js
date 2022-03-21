1dbc9129ef697e65209fdd2187c8e9c4a9e0caba
mzyu19 <7296488+markzyu@users.noreply.github.com>
2022-03-21 10:25:05 -0700

    No need to include the entire MIT license in the dialog

--- src/containers/UserAgreementDialog.js
+++ src/containers/UserAgreementDialog.js
@@ -12,41 +12,14 @@ const UADialog = props => {
     props.dispatch(loadAgreementStatus(getAgreementRead()));
   };
   return (
-    <DismissDialog title="User Agreement" show={props.showAgreement} onDismiss={sendAgree} className="large-modal">
+    <DismissDialog title="User Agreement" show={props.showAgreement} onDismiss={sendAgree} className="medium-modal">
         <em>By using this app, you agree to the following terms. If you do not agree, please DO NOT USE THIS APP.</em>
         <br/>
         <br/>
         Privacy Policy: This app doesn't collect any information remotely. Any data collected by this app is stored locally on your own computer. You can delete such data by clearing cookies and local storage of your browser. This app doesn't target children specifically. IF YOU ARE UNDER THE AGE OF 21, DO NOT USE THIS APP.
         <br/>
         <br/>
-        This software is provided to you via the following LICENSE: 
-        <br/>
-        <br/>
-        MIT License
-        <br/>
-        <br/>
         Copyright (c) 2022 Zhongzhi "Mark" Yu
-        <br/>
-        <br/>
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-        <br/>
-        <br/>
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-        <br/>
-        <br/>
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
     </DismissDialog>
   )
 };
