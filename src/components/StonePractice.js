1697fa76dafe6a8b3fe5dd9c5cca9fb14a333b4b
mzyu19 <7296488+markzyu@users.noreply.github.com>
2023-05-21 10:40:58 -0700

    store stone stats

--- src/components/StonePractice.js
+++ src/components/StonePractice.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import RemarkGfm from 'remark-gfm';
 
 import DismissDialog from './DismissDialog';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { getStoneStats, setStoneStats } from '../store';
 
-export const StoneRow = ({numDots, name, minWidth, prob, setProb}) => {
+export const StoneRow = ({numDots, refTotal, name, minWidth, prob, setProb}) => {
   const [vals, setVals] = useState(new Array(numDots).fill(null));
   const [currIdx, setCurrIdx] = useState(0);
   const markers = vals.map((v, i) => {
@@ -30,6 +33,7 @@ export const StoneRow = ({numDots, name, minWidth, prob, setProb}) => {
   };
 
   const total = vals.filter(v => v).length;
+  refTotal.current = {value: total};
 
   return (
     <div style={{display: 'flex', justifyContent: 'space-around', width: '100%'}}>
@@ -53,13 +57,63 @@ const regulateMinMaxProb = (v) => {
   return v;
 }
 
+const listStones = [
+  '6 6', '7 5', '7 6', '7 7', '9 6', '9 7', 'Total'
+]
+
+const Stats = () => {
+  const stats = getStoneStats() || {};
+  const listStones = [
+    '6 6', '7 5', '7 6', '7 7', '9 6', '9 7', 'Total'
+  ]
+  const list = listStones.map(name => `| ${name} | ${stats[name] || 0} |`).join('\n');
+  const statsMarkdown = `
+  | Stone | Times |
+  | -- | -- |
+  ${list}
+  `;
+
+  return <ReactMarkdown className='noSelects' children={statsMarkdown} remarkPlugins={RemarkGfm}/>;
+};
+
+const addStat = (result1, result2, result3) => {
+  const stats = getStoneStats() || {};
+  for (const txt of listStones) {
+    if (txt === 'Total') continue;
+    const [threshold1, threshold2] = txt.split(' ').map(x => parseInt(x));
+    if (result1 < threshold1) continue;
+    if (result2 < threshold2) continue;
+    if (result3 >= 5) continue;
+    console.log(result1, result2, threshold1, threshold2);
+    stats[txt] = (stats[txt] || 0) + 1;
+  }
+  stats.Total = (stats.Total || 0) + 1;
+  setStoneStats(stats);
+}
+
 export const StonePractice = props => {
   const [numDots, setNumDots] = useState(DEFAULT_DOTS);
   const [stoneRowKey, setStoneRowKey] = useState(0);
+  const [stoneStatsKey, setStoneStatsKey] = useState(0);
   const [prob, setProb] = useState(DEFAULT_PROB);
+  const refStoneRow1 = useRef();
+  const refStoneRow2 = useRef();
+  const refStoneRow3 = useRef();
+  const [history, setHistory] = useState('');
 
   const reset = () => {
+    const vals= [
+      refStoneRow1.current?.value || 0,
+      refStoneRow2.current?.value || 0,
+      refStoneRow3.current?.value || 0,
+    ];
+    
+    if (vals.filter(v => v).length > 0) {
+      addStat(...vals);
+      setHistory(`- ${vals.join(', ')}\n` + history);
+    }
     setStoneRowKey(Date.now());
+    setStoneStatsKey(Date.now());
     setProb(DEFAULT_PROB);
   }
 
@@ -69,13 +123,22 @@ export const StonePractice = props => {
         setNumDots(parseInt(e.target.value) || 0);
         reset();
       }}></input> <br/> <br/>
-      <div key={stoneRowKey}>
-        <StoneRow numDots={numDots} name="1" minWidth={40} setProb={setProb} prob={prob}/> <br/>
-        <StoneRow numDots={numDots} name="2" minWidth={40} setProb={setProb} prob={prob}/> <br/>
-        <StoneRow numDots={numDots} name="R" minWidth={40} setProb={setProb} prob={prob}/> <br/>
+      <div key={`stoneRow-${stoneRowKey}`}>
+        <StoneRow refTotal={refStoneRow1} numDots={numDots} name="1" minWidth={40} setProb={setProb} prob={prob}/> <br/>
+        <StoneRow refTotal={refStoneRow2} numDots={numDots} name="2" minWidth={40} setProb={setProb} prob={prob}/> <br/>
+        <StoneRow refTotal={refStoneRow3} numDots={numDots} name="R" minWidth={40} setProb={setProb} prob={prob}/> <br/>
       </div>
       Probability: {prob}% <br/> <br/>
       <button onClick={reset}>RESET</button> <br/>
+      <hr />
+      <details className='noSelects' >
+        <summary>Stats / History of rolls</summary>
+        <div style={{maxHeight: 150, overflowY: 'scroll', paddingRight: '4px'}}>
+          <Stats key={`stoneStats-${stoneStatsKey}`} />
+          <hr />
+          <ReactMarkdown className='noSelects' children={history || 'No history.'} />
+        </div>
+      </details>
     </DismissDialog>
   )
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,14 @@
+const KEY_STONE_STATS = "datav1-lost-ark-stone-stats";
 const KEY_USER_AGREEMENT_READ = "metav1-user-agreement-read";
 
 export const setAgreementRead = boolVal => {
   window.localStorage.setItem(KEY_USER_AGREEMENT_READ, JSON.stringify(boolVal));
 }
 
+export const setStoneStats = json => {
+  window.localStorage.setItem(KEY_STONE_STATS, JSON.stringify(json));
+}
+
 export const getAgreementRead = () => !!JSON.parse(window.localStorage.getItem(KEY_USER_AGREEMENT_READ));
+
+export const getStoneStats = () => JSON.parse(window.localStorage.getItem(KEY_STONE_STATS));
